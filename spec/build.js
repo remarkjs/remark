1b7967e83de99ab67d99e7d2e81d8ba07fa621d4
wooorm <tituswormer@gmail.com>
2014-07-27 22:56:32 +0200

    Refactored merge; added istanbul ignore coverage comments

--- spec/build.js
+++ spec/build.js
@@ -1,24 +1,25 @@
 var fs = require('fs');
 var mdast = require('../index.js');
 
-console.log(mdast);
-
 fs.readdirSync('./spec/input').filter(function (path) {
     return path.indexOf('.') !== 0;
 }).forEach(function (path) {
     var filename = path.split('.');
     filename.pop();
+    if (filename.length > 1) {
+        console.log('filename', filename);
+    }
     filename = filename.join('.');
 
     var file = fs.readFileSync('./spec/input/' + path, 'utf-8');
     var json;
 
     try {
-        console.log('+' + filename);
+        // console.log('+' + filename);
         json = mdast(file);
         fs.writeFileSync('./spec/output/' + filename + '.json', JSON.stringify(json, null, 2));
     } catch (error) {
-        console.log('-' + filename);
+        // console.log('-' + filename);
         throw error;
         fs.writeFileSync('./spec/output/' + filename.toUpperCase() + '.json', '');
     }
