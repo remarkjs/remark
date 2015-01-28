a9630a134730928c346255699316f200922b24d5
wooorm <tituswormer@gmail.com>
2015-01-28 21:02:31 +0100

    Add `example.js`

---
+++ example.js
@@ -0,0 +1,14 @@
+// See [Nodes](doc/Nodes.md) for information about returned objects.
+var mdast = require('./index.js');
+
+// Parse markdown with `mdast.parse`:
+var ast = mdast.parse('Some *emphasis*,  **strongness**, and `code`.');
+
+// Yields:
+console.log('json', JSON.stringify(ast, 0, 2));
+
+// And passing that document into `mdast.stringify`:
+var doc = mdast.stringify(ast);
+
+// Yields:
+console.log('markdown', doc);
