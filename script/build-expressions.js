d8fe1976a3df87dbc152f2eea5805887ea88cf8c
wooorm <tituswormer@gmail.com>
2015-02-13 11:26:19 +0100

    Add `lib/expressions.js` with precompiled expressions
    - Add `script/build-expressions.js` to build `lib/expressions.js`;
    - Add `build-expressions` npm script target to build expressions;
    - Add `build-expressions` to `build` target.

---
+++ script/build-expressions.js
@@ -0,0 +1,336 @@
+'use strict';
+
+/*
+ * Dependencies.
+ */
+
+var fs = require('fs');
+
+/*
+ * Methods.
+ */
+
+var write = fs.writeFileSync;
+
+/*
+ * Expressions.
+ */
+
+var EXPRESSION = /(^|[^\[])\^/g;
+
+/**
+ * Clean an expression.
+ *
+ * @param {RegExp|string} expression
+ * @return {string}
+ */
+function cleanExpression(expression) {
+    return (expression.source || expression).replace(EXPRESSION, '$1');
+}
+
+/*
+ * Exports.
+ */
+
+var expressions = {};
+
+/*
+ * Block rules;
+ */
+
+var rules = {};
+
+expressions.rules = rules;
+
+/*
+ * Block helpers.
+ */
+
+rules.newline = /^\n+/;
+
+rules.bullet = /(?:[*+-]|\d+\.)/;
+
+rules.code = /^( {4}[^\n]+\n*)+/;
+
+rules.horizontalRule = /^( *[-*_]){3,} *(?=\n|$)/;
+
+rules.heading = /^ *((#{1,6}) *)([^\n]+?) *#* *(?=\n|$)/;
+
+rules.lineHeading = /^([^\n]+)\n *(=|-){2,} *(?=\n|$)/;
+
+rules.linkDefinition =
+    /^ *\[([^\]]+)\]: *<?([^\s>]+)>?(?: +["(]([^\n]+)[")])? *(?=\n|$)/;
+
+rules.blockText = /^[^\n]+/;
+
+rules.item = new RegExp(
+    '^( *)(' +
+        cleanExpression(rules.bullet) +
+    ') [^\\n]*(?:\\n(?!\\1' +
+        cleanExpression(rules.bullet) +
+    ' )[^\\n]*)*',
+'gm');
+
+rules.list = new RegExp(
+    '^' +
+    '( *)' +
+    '(' + cleanExpression(rules.bullet) + ')' +
+    '(' +
+        '(?: [\\s\\S]+?)' +
+        '(?:' +
+
+            /*
+             * Modified Horizontal rule:
+             */
+
+            '\\n+(?=\\1?(?:[-*_] *){3,}(?=\\n|$))' +
+            '|' +
+
+            /*
+             * Modified Link Definition:
+             */
+
+            '\\n+(?=' + cleanExpression(rules.linkDefinition) + ')' +
+            '|' +
+
+            '\\n{2,}(?! )(?!\\1' +
+                cleanExpression(rules.bullet) +
+            ' )' +
+            '|' +
+
+            '\\s*$' +
+        ')' +
+    ')'
+);
+
+rules.blockquote = new RegExp(
+    '^( *>[^\\n]+(\\n(?!' +
+
+    cleanExpression(rules.linkDefinition) +
+
+    ')[^\\n]+)*)+'
+);
+
+var inlineTags;
+
+inlineTags = (
+    '(?!' +
+        '(?:' +
+            'a|em|strong|small|s|cite|q|dfn|abbr|data|time|code|' +
+            'var|samp|kbd|sub|sup|i|b|u|mark|ruby|rt|rp|bdi|bdo|' +
+            'span|br|wbr|ins|del|img' +
+        ')\\b' +
+    ')' +
+    '\\w+(?!:' +
+        '\\/|[^\\w\\s@]*@' +
+    ')\\b'
+);
+
+rules.html = new RegExp(
+    '^ *(?:' +
+
+        /*
+         * HTML comment.
+         */
+
+        cleanExpression('<!--[\\s\\S]*?-->') +
+        ' *(?:\\n|\\s*$)|' +
+
+        /*
+         * Closed tag.
+         */
+
+        cleanExpression('<(' + inlineTags + ')[\\s\\S]+?<\\/\\1>') +
+        ' *(?:\\n{2,}|\\s*$)|' +
+
+        /*
+         * Closing tag.
+         */
+
+        cleanExpression(
+            '<' + inlineTags + '(?:"[^"]*"|\'[^\']*\'|[^\'">])*?>'
+        ) +
+        ' *' +
+        '(?:\\n{2,}|\\s*$)' +
+    ')'
+);
+
+rules.paragraph = new RegExp(
+    '^(?:(?:' +
+        '[^\\n]+\\n?' +
+        '(?!' +
+            cleanExpression(rules.horizontalRule) +
+            '|' +
+            cleanExpression(rules.heading) +
+            '|' +
+            cleanExpression(rules.lineHeading) +
+            '|' +
+            cleanExpression(rules.blockquote) +
+            '|' +
+            cleanExpression('<' + inlineTags) +
+            '|' +
+            cleanExpression(rules.linkDefinition) +
+        ')' +
+    ')+)'
+);
+
+/*
+ * GFM + Tables Block Grammar
+ */
+
+var tables = {};
+
+expressions.tables = tables;
+
+tables.table =
+    /^( *\|(.+))\n( *\|( *[-:]+[-| :]*)\n)((?: *\|.*(?:\n|$))*)/;
+
+tables.looseTable =
+    /^( *(\S.*\|.*))\n( *([-:]+ *\|[-| :]*)\n)((?:.*\|.*(?:\n|$))*)/;
+
+/*
+ * GFM Block Grammar.
+ */
+
+var gfm = {};
+
+expressions.gfm = gfm;
+
+gfm.fences =
+    /^ *(`{3,}|~{3,}) *(\S+)? *\n([\s\S]*?)\s*\1 *(?=\n|$)/;
+
+gfm.paragraph = new RegExp(
+    rules.paragraph.source.replace('(?!', '(?!' +
+        cleanExpression(gfm.fences).replace('\\1', '\\2') +
+        '|' +
+        cleanExpression(rules.list).replace('\\1', '\\3') +
+        '|'
+    )
+);
+
+/*
+ * Footnote block grammar
+ */
+
+var footnotes = {};
+
+expressions.footnotes = footnotes;
+
+footnotes.footnoteDefinition =
+    /^( *\[\^([^\]]+)\]: *)([^\n]+(\n+ +[^\n]+)*)/;
+
+/*
+ * YAML front matter.
+ */
+
+var yaml = {};
+
+expressions.yaml = yaml;
+
+yaml.yamlFrontMatter = /^-{3}\n([\s\S]+?\n)?-{3}/;
+
+/*
+ * Inline-Level Grammar.
+ */
+
+rules.escape = /^\\([\\`*{}\[\]()#+\-.!_>])/;
+
+rules.autoLink = /^<([^ >]+(@|:\/)[^ >]+)>/;
+
+rules.tag = /^<!--[\s\S]*?-->|^<\/?\w+(?:"[^"]*"|'[^']*'|[^'">])*?>/;
+
+rules.invalidLink = /^(!?\[)((?:\[[^\]]*\]|[^\[\]])*)\]/;
+
+rules.strong = /^__([\s\S]+?)__(?!_)|^\*\*([\s\S]+?)\*\*(?!\*)/;
+
+rules.emphasis =
+    /^\b_((?:__|[\s\S])+?)_\b|^\*((?:\*\*|[\s\S])+?)\*(?!\*)/;
+
+rules.inlineCode = /^(`+)\s*([\s\S]*?[^`])\s*\1(?!`)/;
+
+rules.break = /^ {2,}\n(?!\s*$)/;
+
+rules.text = /^[\s\S]+?(?=[\\<!\[_*`]| {2,}\n|$)/;
+
+rules.inside = /(?:\[[^\]]*\]|[^\[\]]|\](?=[^\[]*\]))*/;
+
+rules.href = /\s*<?([\s\S]*?)>?(?:\s+['"]([\s\S]*?)['"])?\s*/;
+
+rules.link = new RegExp(
+    '^(!?\\[)(' +
+        cleanExpression(rules.inside) +
+    ')\\]\\(' +
+        cleanExpression(rules.href) +
+    '\\)'
+);
+
+rules.referenceLink = new RegExp(
+    '^(!?\\[)(' +
+        cleanExpression(rules.inside) +
+    ')\\]\\s*\\[([^\\]]*)\\]'
+);
+
+/*
+ * GFM inline Grammar.
+ */
+
+gfm.escape = new RegExp(
+    rules.escape.source.replace('])', '~|])')
+);
+
+gfm.url = /^(https?:\/\/[^\s<]+[^<.,:;"')\]\s])/;
+
+gfm.deletion = /^~~(?=\S)([\s\S]*?\S)~~/;
+
+gfm.text = new RegExp(
+    rules.text.source.replace(']|', '~]|').replace('|', '|https?:\\/\\/|')
+);
+
+/*
+ * Pedantic Inline Grammar.
+ */
+
+var pedantic = {};
+
+expressions.pedantic = pedantic;
+
+pedantic.strong =
+    /^__(?=\S)([\s\S]*?\S)__(?!_)|^\*\*(?=\S)([\s\S]*?\S)\*\*(?!\*)/;
+
+pedantic.emphasis =
+    /^_(?=\S)([\s\S]*?\S)_(?!_)|^\*(?=\S)([\s\S]*?\S)\*(?!\*)/;
+
+/*
+ * GFM + Line Breaks Inline Grammar
+ */
+
+var breaks = {};
+var breaksGFM = {};
+
+expressions.breaks = breaks;
+expressions.breaksGFM = breaksGFM;
+
+breaks.break = new RegExp(rules.break.source.replace('{2,}', '*'));
+breaks.text = new RegExp(rules.text.source.replace('{2,}', '*'));
+
+breaksGFM.text = new RegExp(
+    gfm.text.source.replace('{2,}', '*')
+);
+
+/*
+ * Write.
+ */
+
+var content;
+
+content = '/* This file is generated by `script/build-expressions.js` */\n' +
+    'module.exports={\n' +
+    Object.keys(expressions).map(function (key) {
+        var map = expressions[key];
+        return '  "' + key + '": {\n' + Object.keys(map).map(function (name) {
+            return '    "' + name + '":' + map[name].toString();
+        }).join(',\n') + '\n  }';
+    }).join(',\n') +
+    '\n};\n';
+
+write('lib/expressions.js', content);
