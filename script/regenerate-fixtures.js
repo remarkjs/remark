5b7ae76e10c0325ae890c953821e0d50a0542bee
Titus Wormer <tituswormer@gmail.com>
2015-03-19 08:20:56 +0100

    Add sceipt to regenerate fixtures
    -   Add `checked: null` attributes;
    -   Add `start: null` and `start: 1` attributes;
    -   Add missing EOF EOL.

---
+++ script/regenerate-fixtures.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var fs = require('fs');
+var mdast = require('..');
+var fixtures = require('../test/fixtures.js');
+
+fixtures.forEach(function (fixture) {
+    var input = fixture.input;
+    var name = fixture.name;
+    var mapping = fixture.mapping;
+
+    Object.keys(mapping).forEach(function (key) {
+        var filename = name + (key ? '.' + key : key) + '.json';
+        var result = mdast.parse(input, fixture.possibilities[key]);
+
+        result = JSON.stringify(result, null, 2) + '\n';
+
+        fs.writeFileSync('test/tree/' + filename, result);
+    });
+});
