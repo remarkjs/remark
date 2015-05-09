remarkjs/remark

commit 609b9380d2183fab2d3f4b5beb8551f1e488a8db

Fix build fail on case-sensitive file systems

script/build-options.js
@@ -34,7 +34,7 @@ var settings = JSON.parse(fs.readFileSync('.mdastrc')).settings;
 
 sections = sections.map(function (filepath) {
     return {
-        'description': fs.readFileSync(filepath + '/Description.md', 'utf8'),
+        'description': fs.readFileSync(filepath + '/description.md', 'utf8'),
         'path': filepath,
         'options': fs.readFileSync(filepath + '/config.json', 'utf8'),
         'fixture': fs.readFileSync(filepath + '/fixture.text', 'utf8'),

commit 6a8d7251fede4c48db54cd5cc24cec8682725b1b

Fix incorrect links introduced in 609b938

.mdastignore
@@ -4,7 +4,7 @@
 components
 
 # Already looks beautiful because it’s generated by mdast :)
-doc/Options.md
+doc/options.md
 
 # Make sure the fixtures do not get overwritten.
 test/{input,tree}

lib/cli/program.js
@@ -134,16 +134,16 @@ function extensions(extension, cache) {
 function help() {
     console.log('  Usage:');
     console.log();
-    console.log('  # Pass `Readme.md` through mdast');
-    console.log('  $ ' + COMMAND + ' Readme.md -o Readme-new.md');
+    console.log('  # Pass `readme.md` through mdast');
+    console.log('  $ ' + COMMAND + ' readme.md -o readme-new.md');
     console.log();
     console.log('  # Pass stdin through mdast, with settings, to stdout');
-    console.log('  $ cat Readme.md | ' + COMMAND + ' -s "setext, bullet: *"' +
-        ' > Readme-new.md');
+    console.log('  $ cat readme.md | ' + COMMAND + ' -s "setext, bullet: *"' +
+        ' > readme-new.md');
     console.log();
     console.log('  # Use a plugin (with options)');
     console.log('  $ npm install mdast-toc');
-    console.log('  $ ' + COMMAND + ' --use toc=heading:contents Readme.md ' +
+    console.log('  $ ' + COMMAND + ' --use toc=heading:contents readme.md ' +
         '-o');
     console.log();
     console.log('  # Rewrite markdown in a directory');

man/mdast.3
@@ -27,7 +27,7 @@ var doc = processor\.process(
 This is the application programming interface documentation for \fBmdast\fR\|\.  To find documentation for the command line interface, see \fBman 1 mdast\fR\|\.
 .SS mdast\.use(plugin, options?)
 .P
-Change the way \fBmdast\fR functions by using a plugin\.  Plugins are documented at https://github\.com/wooorm/mdast/blob/master/doc/Plugins\.md\|\.
+Change the way \fBmdast\fR functions by using a plugin\.  Plugins are documented at https://github\.com/wooorm/mdast/blob/master/doc/plugins\.md\|\.
 .P
 \fBSignatures\fR
 .RS 0
@@ -76,7 +76,7 @@ Parse a markdown document into an abstract syntax tree\.
 .P
 \fBReturns\fR
 .P
-\fBObject\fR \-\- Node\.  Nodes are documented at https://github\.com/wooorm/mdast/blob/master/doc/Nodes\.md\|\.
+\fBObject\fR \-\- Node\.  Nodes are documented at https://github\.com/wooorm/mdast/blob/master/doc/nodes\.md\|\.
 .SS mdast\.run(ast, file, done?)
 .P
 Modify an abstract syntax tree by applying plugins to it\.

man/mdastconfig.7
@@ -19,7 +19,7 @@ Plug\-ins can configure mdast, for example, \fBmdast\-yaml\-config\fR allows per
 
 .RE
 .P
-For a list of available configuration options, see the SETTINGS section below or https://github\.com/wooorm/mdast/blob/master/doc/Options\.md\|\.
+For a list of available configuration options, see the SETTINGS section below or https://github\.com/wooorm/mdast/blob/master/doc/options\.md\|\.
 .SS SETTINGS FOR \fBPROCESS\fR, \fBPARSE()\fR, AND \fBSTRINGIFY()\fR
 .P
 To configure the programatic interface of \fBmdast\fR, pass an object as a second parameter to \fBprocess()\fR, \fBparse()\fR, and \fBstringify()\fR\|\.
@@ -48,7 +48,7 @@ Specify directory specific settings with \fB\|\.mdastrc\fR and \fBpackage\.json\
 .SH SETTINGS
 .SS PARSE
 .P
-See https://github\.com/wooorm/mdast/blob/master/doc/Options\.md#parse for a description of these settings\.
+See https://github\.com/wooorm/mdast/blob/master/doc/options\.md#parse for a description of these settings\.
 .RS 0
 .IP \(bu 2
 \fBgfm\fR (boolean, default: true);
@@ -66,7 +66,7 @@ See https://github\.com/wooorm/mdast/blob/master/doc/Options\.md#parse for a des
 .RE
 .SS STRINGIFY
 .P
-See https://github\.com/wooorm/mdast/blob/master/doc/Options\.md#stringify for a description of these settings\.
+See https://github\.com/wooorm/mdast/blob/master/doc/options\.md#stringify for a description of these settings\.
 .RS 0
 .IP \(bu 2
 \fBentities\fR (\fBfalse\fR, \fBtrue\fR, or \fB"numbers"\fR, default: \fBfalse\fR);

man/mdastrc.5
@@ -8,7 +8,7 @@
 .P
 \fBmdast\fR gets its configuration from the command line and \fBmdastrc\fR files\.
 .P
-For a list of available configuration options, see \fBmdast\fR(1) or https://github\.com/wooorm/mdast/blob/master/doc/Options\.md\|\.
+For a list of available configuration options, see \fBmdast\fR(1) or https://github\.com/wooorm/mdast/blob/master/doc/options\.md\|\.
 .SH FILES
 .P
 All \fBmdastrc\fR(5) configuration files are in JSON\.
@@ -44,7 +44,7 @@ List:
 .fi
 .RE
 .P
-Options
+Options:
 .P
 .RS 2
 .nf
@@ -84,11 +84,11 @@ For example, for the following project:
 project
 |\-\- docs
 |   |\-\- \.mdastrc
-|   |\-\- Doc\.md
+|   |\-\- doc\.md
 |
 |\-\- \.mdastrc
 |\-\- package\.json
-|\-\- Readme\.md
+|\-\- readme\.md
 .fi
 .RE
 .P
@@ -130,9 +130,9 @@ And \fB\|\.mdastrc\fR contains:
 .fi
 .RE
 .P
-Then, when stringifying \fBdocs/Doc\.md\fR, \fBmdast\fR(1) would use \fBbullet: "+"\fR because \fBdocs/\.mdastrc\fR takes precedence over \fB\|\.mdastrc\fR and \fBpackage\.json\fR\|\.
+Then, when stringifying \fBdocs/doc\.md\fR, \fBmdast\fR(1) would use \fBbullet: "+"\fR because \fBdocs/\.mdastrc\fR takes precedence over \fB\|\.mdastrc\fR and \fBpackage\.json\fR\|\.
 .P
-When stringifying \fBReadme\.md\fR, \fBmdast\fR(1) would use \fBbullet: "\-"\fR, because \fB\|\.mdastrc\fR takes precedence over \fBpackage\.json\fR\|\.
+When stringifying \fBreadme\.md\fR, \fBmdast\fR(1) would use \fBbullet: "\-"\fR, because \fB\|\.mdastrc\fR takes precedence over \fBpackage\.json\fR\|\.
 .SH BUGS
 .P
 https://github\.com/wooorm/mdast/issues

script/build-options.js
@@ -97,7 +97,7 @@ root = mdast.parse(
         'master/doc/mdast.3.md#mdaststringifyast-options).\n' +
     '\n' +
     'Information on **mdast** itself is available in the project’s ' +
-        '[Readme.md](https://github.com/wooorm/mdast#readme).\n' +
+        '[readme.md](https://github.com/wooorm/mdast#readme).\n' +
     '\n' +
     '## Table of Contents\n' +
     '\n'
@@ -242,4 +242,4 @@ toc()(root);
  * Write.
  */
 
-fs.writeFileSync('doc/Options.md', mdast.stringify(root, settings));
+fs.writeFileSync('doc/options.md', mdast.stringify(root, settings));

test/index.js
@@ -971,10 +971,10 @@ describe('File(options?)', function () {
 
         it('should add properties', function () {
             var err = new File({
-                'filename': 'Untitled'
+                'filename': 'untitled'
             }).exception('test');
 
-            assert(err.file === 'Untitled.md');
+            assert(err.file === 'untitled.md');
             assert(err.reason === 'test');
             assert(err.line === null);
             assert(err.column === null);
@@ -1000,7 +1000,7 @@ describe('File(options?)', function () {
         it('should include the filename in `toString()`', function () {
             assert(new File({
                 'filename': 'Untitled'
-            }).exception('test').toString() === 'Untitled.md:1:1: test');
+            }).exception('test').toString() === 'untitled.md:1:1: test');
         });
 
         it('should accept an error', function () {