remarkjs/remark

commit 517d483ffdf9d1e9b813bdb28913c152e41ccb1e

Refactor to externalise file finding
*   Refactor to `vfile-find-up`, `vfile-find-down` for
    more robust, tested, and async file finding;
*   Add proper globbing support.

lib/cli/ignore.js
@@ -18,7 +18,7 @@
 var fs = require('fs');
 var path = require('path');
 var debug = require('debug')('mdast:cli:ignore');
-var Finder = require('./finder');
+var findUp = require('vfile-find-up');
 
 /*
  * Constants.
@@ -102,7 +102,6 @@ function Ignore(options) {
 
     self.cache = {};
     self.cwd = options.cwd || process.cwd();
-    self.finder = new Finder([IGNORE_NAME], self.cwd);
 
     self.detectIgnore = settings.detectIgnore;
 
@@ -123,9 +122,10 @@ function Ignore(options) {
  * @this {Ignore}
  * @param {string?} [filePath] - Path to file, defaults to
  *   current working directory.
- * @return {Array.<string>} - Array of ignored paths.
+ * @param {Function} callback - Invoked with an array of
+ *   ignored paths.
  */
-function getPatterns(filePath) {
+function getPatterns(filePath, callback) {
     var self = this;
     var directory = filePath ? path.dirname(filePath) : self.cwd;
     var ignore = self.cache[directory];
@@ -139,18 +139,38 @@ function getPatterns(filePath) {
     } else if (!self.detectIgnore) {
         ignore = self.defaults;
     } else if (!has.call(self.cache, directory)) {
-        ignore = self.finder.find(directory, true);
+        findUp.one([IGNORE_NAME], directory, function (err, file) {
+            var result;
 
-        ignore = (ignore ? load(ignore) : []).concat(self.defaults);
+            if (err) {
+                callback(err);
+                return;
+            }
 
-        self.cache[directory] = ignore;
+            try {
+                result = load(file && file.filePath());
+            } catch (err) {
+                callback(err);
+                return;
+            }
+
+            ignore = (result ? result : []).concat(self.defaults);
+
+            self.cache[directory] = ignore;
+
+            callback(null, ignore)
+        });
+
+        ignore = null;
     } else {
         debug('Using ignore from cache');
 
         ignore = self.cache[directory];
     }
 
-    return ignore;
+    if (ignore) {
+        callback(null, ignore);
+    }
 }
 
 /*

lib/cli/configuration.js
@@ -19,9 +19,9 @@ var fs = require('fs');
 var path = require('path');
 var debug = require('debug')('mdast:cli:configuration');
 var home = require('user-home');
+var findUp = require('vfile-find-up');
 var extend = require('extend.js');
 var defaults = require('../defaults');
-var Finder = require('./finder');
 
 /*
  * Constants.
@@ -29,7 +29,8 @@ var Finder = require('./finder');
 
 var RC_NAME = '.mdastrc';
 var PLUGIN_KEY = 'plugins';
-var PACKAGE_NAME = 'package.json';
+var PACKAGE_NAME = 'package';
+var PACKAGE_EXTENSION = 'json';
 var PACKAGE_FIELD = 'mdastConfig';
 var PERSONAL_CONFIGURATION = home ? path.join(home, RC_NAME) : null;
 
@@ -168,39 +169,43 @@ function getUserConfiguration() {
  *
  * @param {Configuration} context - Configuration object to use.
  * @param {string} directory - Location to search.
- * @return {Object} - Settings object.
+ * @param {Function} callback - Invoked with `files`.
  */
-function getLocalConfiguration(context, directory) {
-    var files = context.finder.find(directory);
-    var configuration = {};
-    var index = files.length;
-    var file;
-    var local;
-    var found;
+function getLocalConfiguration(context, directory, callback) {
+    findUp.all([RC_NAME, PACKAGE_NAME], directory, function (err, files) {
+        var configuration = {};
+        var index = files && files.length;
+        var file;
+        var local;
+        var found;
 
-    while (index--) {
-        file = files[index];
+        while (index--) {
+            file = files[index];
 
-        local = load(file);
+            local = load(file.filePath());
 
-        if (path.basename(file) === PACKAGE_NAME) {
-            local = local[PACKAGE_FIELD] || {};
+            if (
+                file.filename === PACKAGE_NAME &&
+                file.extension === PACKAGE_EXTENSION
+            ) {
+                local = local[PACKAGE_FIELD] || {};
+            }
+
+            found = true;
+
+            debug('Using ' + file.filePath());
+
+            merge(configuration, local);
         }
 
-        found = true;
+        if (!found) {
+            debug('Using personal configuration');
 
-        debug('Using ' + file);
+            merge(configuration, getUserConfiguration());
+        }
 
-        merge(configuration, local);
-    }
-
-    if (!found) {
-        debug('Using personal configuration');
-
-        merge(configuration, getUserConfiguration());
-    }
-
-    return configuration;
+        callback(err, configuration);
+    });
 }
 
 /**
@@ -235,8 +240,6 @@ function Configuration(options) {
     }
 
     self.cliConfiguration = cliConfiguration;
-
-    self.finder = new Finder([RC_NAME, PACKAGE_NAME], self.cwd);
 }
 
 /**
@@ -250,27 +253,26 @@ Configuration.prototype.base = base;
  * Build a configuration object.
  *
  * @example
- *   var config = new Configuration().getConfiguration('~/foo');
+ *   new Configuration().getConfiguration('~/foo', console.log);
  *
  * @param {string} filePath - File location.
- * @return {Object} - Settings object.
  */
-Configuration.prototype.getConfiguration = function (filePath) {
+Configuration.prototype.getConfiguration = function (filePath, callback) {
     var self = this;
     var directory = filePath ? path.dirname(filePath) : self.cwd;
     var configuration = self.cache[directory];
 
     debug('Constructing configuration for `' + (filePath || self.cwd) + '`');
 
-    if (!configuration) {
-        configuration = {};
-
-        merge(configuration, self.base);
-
-        if (!self.detectRC) {
-            debug('Ignoring .rc files');
-        } else {
-            merge(configuration, getLocalConfiguration(self, directory));
+    /**
+     * Handle (possible) local config result.
+     *
+     * @param {Error?} [err] - Loading error.
+     * @param {Object?} [localConfiguration] - Configuration.
+     */
+    function handleLocalConfiguration(err, localConfiguration) {
+        if (localConfiguration) {
+            merge(configuration, localConfiguration);
         }
 
         merge(configuration, self.cliConfiguration);
@@ -282,11 +284,25 @@ Configuration.prototype.getConfiguration = function (filePath) {
         });
 
         self.cache[directory] = configuration;
-    } else {
-        debug('Using configuration from cache');
+
+        callback(err, configuration);
     }
 
-    return configuration;
+    if (configuration) {
+        debug('Using configuration from cache');
+        callback(null, configuration);
+    } else {
+        configuration = {};
+
+        merge(configuration, self.base);
+
+        if (!self.detectRC) {
+            debug('Ignoring .rc files');
+            handleLocalConfiguration();
+        } else {
+            getLocalConfiguration(self, directory, handleLocalConfiguration);
+        }
+    }
 };
 
 /*

lib/cli/file-pipeline/configure.js
@@ -155,47 +155,58 @@ function findPlugin(pathlike, cwd) {
  *
  * @param {Object} context
  */
-function configure(context) {
+function configure(context, next) {
     var file = context.file;
     var cli = context.fileSet.cli;
     var config = cli.configuration;
-    var options = config.getConfiguration(file.filePath());
     var processor = mdast();
     var plugins;
 
     if (file.hasFailed()) {
+        next();
         return;
     }
 
-    debug('Setting output `%s`', options.output);
+    config.getConfiguration(file.filePath(), function (err, options) {
+        debug('Setting output `%s`', options.output);
 
-    debug('Using settings `%j`', options.settings);
+        debug('Using settings `%j`', options.settings);
 
-    plugins = Object.keys(options.plugins);
+        plugins = Object.keys(options.plugins);
 
-    debug('Using plug-ins `%j`', plugins);
+        debug('Using plug-ins `%j`', plugins);
 
-    /*
-     * Use, with options.
-     */
+        /*
+         * Use, with options.
+         */
 
-    plugins.forEach(function (name) {
-        var option = options.plugins[name];
-        var plugin = findPlugin(name, cli.cwd);
+        plugins.forEach(function (name) {
+            var option = options.plugins[name];
+            var plugin;
 
-        if (option === false) {
-            debug('Ignoring plug-in `%s`', name);
-            return;
-        }
+            if (option === false) {
+                debug('Ignoring plug-in `%s`', name);
+                return;
+            }
 
-        debug('Applying options `%j` to `%s`', option, name);
+            try {
+                plugin = findPlugin(name, cli.cwd);
 
-        processor.use(plugin, option, context.fileSet);
+                debug('Applying options `%j` to `%s`', option, name);
+
+                processor.use(plugin, option, context.fileSet);
+            } catch (err) {
+                next(err)
+                return false;
+            }
+        });
+
+        context.output = options.output;
+        context.settings = options.settings;
+        context.processor = processor;
+
+        next();
     });
-
-    context.output = options.output;
-    context.settings = options.settings;
-    context.processor = processor;
 }
 
 /*

lib/cli/file-pipeline/file-system.js
@@ -36,7 +36,7 @@ var writeFile = fs.writeFile;
  *     'contents': '# Hello'
  *   });
  *
- *   file.providedByUser = true;
+ *   file.given = true;
  *
  *   fileSystem({
  *     'output': true,
@@ -61,7 +61,7 @@ function fileSystem(context, done) {
         return;
     }
 
-    if (!file.namespace('mdast:cli').providedByUser) {
+    if (!file.namespace('mdast:cli').given) {
         debug('Ignoring programmatically added file');
 
         done();

lib/cli/file-pipeline/stdout.js
@@ -51,7 +51,7 @@ function stdout(context) {
         fileSet.length === 1 &&
         (!context.output || !file.filePath())
     ) {
-        if (!file.namespace('mdast:cli').providedByUser) {
+        if (!file.namespace('mdast:cli').given) {
             debug('Ignoring programmatically added file');
 
             return;

lib/cli/file-set-pipeline/file-system.js
@@ -1,0 +1,61 @@
+/**
+ * @author Titus Wormer
+ * @copyright 2015 Titus Wormer
+ * @license MIT
+ * @module mdast:cli:file-set-pipeline:traverse
+ * @version 2.1.0
+ * @fileoverview Find files from the file-system.
+ */
+
+'use strict';
+
+/* eslint-env node */
+
+/*
+ * Dependencies.
+ */
+
+var Ignore = require('../ignore');
+var Finder = require('../finder');
+
+/**
+ * Find files from the file-system.
+ *
+ * @example
+ *   traverse({
+ *     'extensions': ['markdown']
+ *   });
+ *
+ * @param {CLI} cli
+ */
+function traverse(cli, next) {
+    /** Traverse. */
+    function done() {
+        cli.traverser.find(cli.globs, function (err, files) {
+            cli.files = files || [];
+            next(err);
+        });
+    }
+
+    if (cli.traverser) {
+        done();
+    } else {
+        new Ignore({
+            'file': cli.ignorePath,
+            'detectIgnore': cli.detectIgnore
+        }).getPatterns(null, function (err, patterns) {
+            cli.traverser = new Finder({
+                'extensions': cli.extensions,
+                'ignore': patterns || []
+            });
+
+            done();
+        });
+    }
+}
+
+/*
+ * Expose.
+ */
+
+module.exports = traverse;

lib/cli/file-set-pipeline/index.js
@@ -17,7 +17,7 @@
 
 var ware = require('ware');
 var configure = require('./configure');
-var traverse = require('./traverse');
+var fileSystem = require('./file-system');
 var stdin = require('./stdin');
 var transform = require('./transform');
 var log = require('./log');
@@ -28,7 +28,7 @@ var log = require('./log');
 
 var fileSetPipeline = ware()
     .use(configure)
-    .use(traverse)
+    .use(fileSystem)
     .use(stdin)
     .use(transform)
     .use(log);

lib/cli/file-set-pipeline/log.js
@@ -24,8 +24,8 @@ var report = require('vfile-reporter');
  * @param {VFile} file - Virtual file.
  * @return {boolean} - Whether given by user.
  */
-function providedByUser(file) {
-    return file.namespace('mdast:cli').providedByUser;
+function given(file) {
+    return file.namespace('mdast:cli').given;
 }
 
 /**
@@ -34,9 +34,7 @@ function providedByUser(file) {
  * @param {CLI} context - CLI engine.
  */
 function log(context) {
-    var files = context.files;
-    var applicables = files.filter(providedByUser);
-    var diagnostics = report(applicables, {
+    var diagnostics = report(context.files.filter(given), {
         'quiet': context.quiet,
         'silent': context.silent
     });

commit 7233fb2c6adcf2bdfe2ae52fecc645947f022b68

Fix silent exit when without `.mdastignore` file
The externalisation of file finding, in 517d483, introduced
a bug where when an ignore file could not be found the process
exited quietly.

Related to GH-90.

lib/cli/ignore.js
@@ -65,7 +65,7 @@ function isApplicable(value) {
  * @return {Object} - List of applicable patterns.
  */
 function load(filePath) {
-    var ignore = {};
+    var ignore = [];
 
     if (filePath) {
         try {