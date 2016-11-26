51c834beaa10743b3652163eaec43b7090d0eb06
Titus Wormer <tituswormer@gmail.com>
2016-11-26 14:15:51 +0100

    Remove `duo`

--- demo.js
+++ demo.js
@@ -1,311 +1,280 @@
 'use strict';
 
-/*
- * Dependencies.
- */
+/* eslint-env browser */
+/* eslint-disable camelcase, guard-for-in, import/no-unassigned-import */
 
-const remark = require('wooorm/remark@3.x');
-const remarkRange = require('wooorm/remark-range@2.x');
-const vfile = require('wooorm/vfile');
-const debounce = require('component/debounce@1.0.0');
-const assign = require('sindresorhus/object-assign');
-const keycode = require('timoxley/keycode');
-const query = require('component/querystring');
-const events = require('component/event');
-const jquery = require('components/jquery');
-const jstree = require('vakata/jstree');
-const escapeHtml = require('component/escape-html');
+var remark = require('remark')();
+var vfile = require('vfile');
+var debounce = require('debounce');
+var assign = require('object-assign');
+var keycode = require('keycode');
+var query = require('component-querystring');
+var events = require('component-event');
+var jquery = require('jquery');
+var escapeHtml = require('escape-html');
 
-/*
- * Constants.
- */
+require('jstree');
 
-const defaultText = `Here’s a tiny demo for __remark__.
+var defaultText = [
+  'Here’s a tiny demo for __remark__.',
+  '',
+  'Its focus is to *showcase* how the options above work.',
+  '',
+  'Cheers!',
+  '',
+  '---',
+  '',
+  'P.S. You can also permalink the current document using `⌘+s` or `Ctrl+s`.',
+  ''
+].join('\n');
 
-Its focus is to *showcase* how the options above work.
+var $write = document.getElementById('write');
+var $read = document.getElementById('read');
+var $readTree = document.getElementById('read-tree');
+var $position = document.getElementsByName('position')[0];
+var $output = document.getElementsByName('output')[0];
+var $stringify = document.querySelectorAll('.stringify');
+var $settings = document.getElementById('settings');
+var $toggleSettings = document.getElementById('toggle-settings');
+var $clear = document.getElementById('clear');
+var $permalink = document.getElementById('permalink');
 
-Cheers!
+// var $options = [].concat(
+//   [].slice.call(document.getElementsByTagName('input')),
+//   [].slice.call(document.getElementsByTagName('select'))
+// );
 
----
+var options = {};
 
-P.S. You can also permalink the current document using \`⌘+s\` or \`Ctrl+s\`.
-`;
+var debouncedChange = debounce(onchange, 20);
 
-/*
- * DOM elements.
- */
+var SHORTCUT_MAP = {s: setPermalink};
 
-const $write = document.getElementById('write');
-const $read = document.getElementById('read');
-const $readTree = document.getElementById('read-tree');
-const $position = document.getElementsByName('position')[0];
-const $output = document.getElementsByName('output')[0];
-const $stringify = document.querySelectorAll('.stringify');
-const $settings = document.getElementById('settings');
-const $toggleSettings = document.getElementById('toggle-settings');
-const $clear = document.getElementById('clear');
-const $permalink = document.getElementById('permalink');
-
-const $options = [].concat(
-    [].slice.call(document.getElementsByTagName('input')),
-    [].slice.call(document.getElementsByTagName('select'))
-);
-
-/*
- * Options.
- */
-
-const options = {};
-
-/**
- * Make jstree-formatted tree from mdast tree.
- */
-function makeJstree(node) {
-    let text;
-
-    if (node.type != 'text') {
-        text = node.type;
-        const props = Object.keys(node)
-                  .filter((key) => (
-                      ['type', 'value', 'children', 'position'].indexOf(key) < 0 ||
-                          (key == 'value' && node.type != 'text')
-                  ))
-                  .map((key) => `${key}=${JSON.stringify(node[key])}`);
-
-        if (props.length) {
-            text += `(${props.join(', ')})`;
-        }
-    }
-    else {
-        text = JSON.stringify(node.value);
-    }
-
-    text = escapeHtml(text);
-
-    return assign({
-        text,
-        a_attr: {
-            title: text
-        },
-        data: {
-            position: node.position
-        }
-    }, node.children && {
-        children: node.children.map(makeJstree)
-    });
-}
-
-/**
- * Change.
- */
-function onchange() {
-    const isTree = options.output === 'tree';
-    const isAST = options.output === 'ast';
-
-    $readTree.style.display = isTree ? '' : 'none';
-    $read.style.display = isTree ? 'none' : '';
-
-    if (!isTree) {
-        const fn = isAST ? 'parse' : 'process';
-        const value = remark[fn]($write.value, options);
-        $read.value = isAST ? JSON.stringify(value, 0, 2) : value;
-    } else {
-        const file = vfile($write.value);
-        let ast = remark.parse(file, assign({}, options, { position: true }));
-        ast = remark.use(remarkRange).run(ast, file);
-
-        jquery($readTree)
-            .jstree('destroy')
-            .off('.jstree')
-            .on('hover_node.jstree', function (ev, data) {
-                const position = data.node.data.position;
-                $write.focus();
-                $write.setSelectionRange(position.start.offset, position.end.offset);
-            })
-            .on('dehover_node.jstree', function (ev, data) {
-                $write.setSelectionRange(0, 0);
-            })
-            .jstree({
-                core: {
-                    data: assign(makeJstree(ast), {
-                        state: {
-                            opened: true
-                        }
-                    }),
-                    themes: {
-                        name: 'proton',
-                        responsive: true
-                    }
-                },
-                conditionalselect: () => false,
-                plugins: ['wholerow', 'conditionalselect']
-            });
-    }
-}
-
-/**
- * Get permalink.
- */
-function getPermalink() {
-    const variables = query.parse(window.location.search);
-
-    for (let key in variables) {
-        if (key === 'text') return variables[key];
-    }
-
-    return null;
-}
-
-/**
- * Get permalink.
- */
-function setPermalink() {
-    const variables = query.parse(window.location.search);
-
-    variables.text = $write.value || '';
-
-    window.location.search = '?' + query.stringify(variables);
-}
-
-const debouncedChange = debounce(onchange, 20);
-
-/*
- * Setting changes.
- */
-
-function targetType($target) {
-    const type = $target.hasAttribute('type') ? $target.type : $target.nodeName.toLowerCase();
-    return type in getTargetValue ? type : 'text';
-}
-
-const getTargetValue = {
-    text: ($target) => $target.value,
-    number: ($target) => Number($target.value),
-    checkbox: ($target) => $target.checked,
-    select: ($target) => $target.selectedIndex >= 0 && $target.selectedOptions[0].value
+var getTargetValue = {
+  text: function ($target) {
+    return $target.value;
+  },
+  number: function ($target) {
+    return Number($target.value);
+  },
+  checkbox: function ($target) {
+    return $target.checked;
+  },
+  select: function ($target) {
+    return $target.selectedIndex >= 0 && $target.selectedOptions[0].value;
+  }
 };
 
-const setTargetValue = {
-    text: ($target, value) => $target.value = value,
-    number: ($target, value) => $target.value = Number(value),
-    checkbox: ($target, value) => $target.checked = value,
-    select: ($target, value) => {
-        const $option = $target.querySelector(`[value="${value}"]`);
-        $target.selectedIndex = $option ? $option.index : -1;
-    }
+var setTargetValue = {
+  text: function ($target, value) {
+    $target.value = value;
+  },
+  number: function ($target, value) {
+    $target.value = Number(value);
+  },
+  checkbox: function ($target, value) {
+    $target.checked = value;
+  },
+  select: function ($target, value) {
+    var $option = $target.querySelector('[value="' + value + '"]');
+    $target.selectedIndex = $option ? $option.index : -1;
+  }
 };
 
-function initializeOptions() {
-    for (let name in localStorage) {
-        const $target = document.getElementsByName(name)[0];
-        if (!$target) continue;
-
-        const type = targetType($target);
-        let value = localStorage.getItem(name);
-        if (type == 'checkbox') {
-            value = value && value != 'false';
-        }
-
-        setTargetValue[type]($target, value);
-        options[name] = value;
-    }
-}
-
-function setOption(name, value) {
-    options[name] = value;
-    localStorage.setItem(name, value);
-}
-
-function onsettingchange(event) {
-    const $target = event.target;
-    const type = targetType($target);
-
-    if (!$target.hasAttribute('name')) return;
-
-    setOption($target.name, getTargetValue[type]($target));
-
-    debouncedChange();
-}
-
-function onmethodchange() {
-    const $option = $output.selectedOptions[0];
-    const compiled = $option && $option.value === 'markdown';
-    const isTree = $option && $option.value === 'tree';
-    const length = $stringify.length;
-    let index = -1;
-
-    while (++index < length) {
-        $stringify[index].disabled = !compiled;
-    }
-
-    $position.checked = isTree ? true : $position.checked;
-    $position.disabled = isTree;
-}
-
-/*
- * Intents.
- */
-
-function onintenttoggelsettings() {
-    var hidden = $settings.classList.contains('hidden');
-
-    $settings.classList[hidden ? 'remove' : 'add']('hidden');
-    $toggleSettings.textContent = (hidden ? 'Hide' : 'Show') + ' settings';
-}
-
-function onintentclear() {
-    $write.value = '';
-    onchange();
-}
-
-function onintentpermalink() {
-    setPermalink();
-}
-
-const SHORTCUT_MAP = {
-    's': onintentpermalink
-};
-
-function onkeydown(event) {
-    var key = keycode(event);
-
-    if (!event.metaKey) {
-        return;
-    }
-
-    if (key in SHORTCUT_MAP) {
-        SHORTCUT_MAP[key]();
-        event.preventDefault();
-    }
-}
-
+/* Listen. */
 events.bind(document, 'keydown', onkeydown);
-
 events.bind($toggleSettings, 'click', onintenttoggelsettings);
 events.bind($clear, 'click', onintentclear);
-events.bind($permalink, 'click', onintentpermalink);
-
-/*
- * Listen.
- */
-
+events.bind($permalink, 'click', setPermalink);
 events.bind(window, 'change', onsettingchange);
 events.bind($output, 'change', onmethodchange);
-
 events.bind($write, 'change', debouncedChange);
 events.bind($write, 'onpropertychange', debouncedChange);
 events.bind($write, 'input', debouncedChange);
 
-/*
- * Initial answer.
- */
-
+/* Initial answer. */
 initializeOptions();
 $write.value = getPermalink() || defaultText;
 onchange();
 
-/*
- * Focus editor.
- */
-
+/* Focus editor. */
 $write.focus();
+
+/* Intents. */
+function onintenttoggelsettings() {
+  var hidden = $settings.classList.contains('hidden');
+
+  $settings.classList[hidden ? 'remove' : 'add']('hidden');
+  $toggleSettings.textContent = (hidden ? 'Hide' : 'Show') + ' settings';
+}
+
+function onintentclear() {
+  $write.value = '';
+  onchange();
+}
+
+function onkeydown(ev) {
+  var key = keycode(ev);
+
+  if (ev.metaKey && key in SHORTCUT_MAP) {
+    SHORTCUT_MAP[key]();
+    ev.preventDefault();
+  }
+}
+
+/* Change. */
+function onchange() {
+  var isTree = options.output === 'tree';
+  var isAST = options.output === 'ast';
+  var fn;
+  var value;
+  var file;
+  var ast;
+
+  $readTree.style.display = isTree ? '' : 'none';
+  $read.style.display = isTree ? 'none' : '';
+
+  if (isTree) {
+    file = vfile($write.value);
+    ast = remark.run(remark.parse(file, assign({}, options, {position: true})));
+
+    jquery($readTree)
+      .jstree('destroy')
+      .off('.jstree')
+      .on('hover_node.jstree', function (ev, data) {
+        var position = data.node.data.position;
+        $write.focus();
+        $write.setSelectionRange(position.start.offset, position.end.offset);
+      })
+      .on('dehover_node.jstree', function () {
+        $write.setSelectionRange(0, 0);
+      })
+      .jstree({
+        core: {
+          data: assign(makeJstree(ast), {state: {opened: true}}),
+          themes: {name: 'proton', responsive: true}
+        },
+        conditionalselect: function () {
+          return false;
+        },
+        plugins: ['wholerow', 'conditionalselect']
+      });
+  } else {
+    fn = isAST ? 'parse' : 'process';
+    value = remark[fn]($write.value, options);
+    $read.value = isAST ? JSON.stringify(value, 0, 2) : value;
+  }
+}
+
+/* Make jstree-formatted tree from mdast tree. */
+function makeJstree(node) {
+  var keys = ['type', 'value', 'children', 'position'];
+  var text;
+  var props;
+
+  if (node.type === 'text') {
+    text = JSON.stringify(node.value);
+  } else {
+    text = node.type;
+    props = Object.keys(node)
+      .filter(function (key) {
+        return keys.indexOf(key) === -1 || (key === 'value' && node.type !== 'text');
+      })
+      .map(function (key) {
+        return key + '=' + JSON.stringify(node[key]);
+      });
+
+    if (props.length !== 0) {
+      text += '(' + props.join(', ') + ')';
+    }
+  }
+
+  text = escapeHtml(text);
+
+  return assign({
+    text: text,
+    a_attr: {title: text},
+    data: {position: node.position}
+  }, node.children && {children: node.children.map(makeJstree)});
+}
+
+function setOption(name, value) {
+  options[name] = value;
+  localStorage.setItem(name, value);
+}
+
+function onsettingchange(event) {
+  var $target = event.target;
+  var type = targetType($target);
+
+  if ($target.hasAttribute('name')) {
+    setOption($target.name, getTargetValue[type]($target));
+    debouncedChange();
+  }
+}
+
+function onmethodchange() {
+  var $option = $output.selectedOptions[0];
+  var compiled = $option && $option.value === 'markdown';
+  var isTree = $option && $option.value === 'tree';
+  var length = $stringify.length;
+  var index = -1;
+
+  while (++index < length) {
+    $stringify[index].disabled = !compiled;
+  }
+
+  $position.checked = isTree ? true : $position.checked;
+  $position.disabled = isTree;
+}
+
+function initializeOptions() {
+  var name;
+  var $target;
+  var type;
+  var value;
+
+  for (name in localStorage) {
+    $target = document.getElementsByName(name)[0];
+
+    if (!$target) {
+      continue;
+    }
+
+    type = targetType($target);
+    value = localStorage.getItem(name);
+
+    if (type === 'checkbox') {
+      value = value && value !== 'false';
+    }
+
+    setTargetValue[type]($target, value);
+    options[name] = value;
+  }
+}
+
+/* Get permalink. */
+function getPermalink() {
+  var variables = query.parse(window.location.search);
+  var key;
+
+  for (key in variables) {
+    if (key === 'text') {
+      return variables[key];
+    }
+  }
+
+  return null;
+}
+
+/* Set permalink. */
+function setPermalink() {
+  var variables = query.parse(window.location.search);
+  variables.text = $write.value || '';
+  window.location.search = '?' + query.stringify(variables);
+}
+
+function targetType($target) {
+  var type = $target.hasAttribute('type') ? $target.type : $target.nodeName.toLowerCase();
+  return type in getTargetValue ? type : 'text';
+}
