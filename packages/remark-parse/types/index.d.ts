55163c930164e652de2acdeda8a135433f8b8868
Mike North <michael.l.north@gmail.com>
2019-01-14 23:22:44 -0800

    chore: respond to code review feedback

--- packages/remark-parse/types/index.d.ts
+++ packages/remark-parse/types/index.d.ts
@@ -1,5 +1,43 @@
 // TypeScript Version: 3.0
 
+declare module 'remark-parse/types' {
+  import {Parser} from 'unified'
+  import {Node, Position} from 'unist'
+
+  interface RemarkParseOptions {
+    gfm: boolean
+    commonmark: boolean
+    footnotes: boolean
+    pedantic: boolean
+  }
+  interface Add {
+    (n: Node, parent?: Node): Node
+    test(): Position
+    reset(n: Node, parent?: Node): Node
+  }
+
+  type Eat = (value: string) => Add
+
+  type Locator = (value: string, fromIndex: number) => number
+
+  interface Tokenizer {
+    (e: Eat, value: string, silent: true): boolean
+    (e: Eat, value: string): Node
+    locator: Locator
+    onlyAtStart: boolean
+    notInBlock: boolean
+    notInList: boolean
+    notInLink: boolean
+  }
+
+  class MDParser extends Parser {
+    blockMethods: string[]
+    inlineTokenizers: {
+      [k: string]: Tokenizer
+    }
+  }
+}
+
 declare module 'remark-parse' {
   import {MDParser, RemarkParseOptions} from 'remark-parse/types'
   import {Attacher} from 'unified'
