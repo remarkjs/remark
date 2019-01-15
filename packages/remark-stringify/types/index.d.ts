b6b70b8f8de1b42a2faad20f6544ffc110c26860
Mike North <michael.l.north@gmail.com>
2019-01-14 23:13:53 -0800

    chore: respond to code review feedback

--- packages/remark-stringify/types/index.d.ts
+++ packages/remark-stringify/types/index.d.ts
@@ -1,5 +1,29 @@
 // TypeScript Version: 3.0
 
+declare module 'remark-stringify/types' {
+  interface RemarkStringifyOptions {
+    gfm: boolean
+    commonmark: boolean
+    entities: boolean | 'numbers' | 'escape'
+    setext: boolean
+    closeAtx: boolean
+    looseTable: boolean
+    spacedTable: boolean
+    paddedTable: boolean
+    stringLength: (s: string) => number
+    fence: '~' | '`'
+    fences: boolean
+    bullet: '-' | '*' | '+'
+    listItemIndent: 'tab' | '1' | 'mixed'
+    incrementListMarker: boolean
+    rule: '-' | '_' | '*'
+    ruleRepetition: number
+    ruleSpaces: boolean
+    strong: '_' | '*'
+    emphasis: '_' | '*'
+  }
+}
+
 declare module 'remark-stringify' {
   import {RemarkStringifyOptions} from 'remark-stringify/types'
   import {Attacher, Compiler, Processor} from 'unified'
