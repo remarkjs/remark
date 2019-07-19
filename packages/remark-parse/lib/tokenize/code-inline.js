69ff5ee7fc0eae8e658efd2093d957ec00e03bdf
Titus Wormer <tituswormer@gmail.com>
2019-07-19 13:03:54 +0200

    Fix merge

--- packages/remark-parse/lib/tokenize/code-inline.js
+++ packages/remark-parse/lib/tokenize/code-inline.js
@@ -1,7 +1,6 @@
 'use strict'
 
 var locate = require('../locate/code-inline')
-var whitespace = require('../util/is-markdown-whitespace-character')
 
 module.exports = inlineCode
 inlineCode.locator = locate
