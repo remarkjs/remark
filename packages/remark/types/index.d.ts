de864137789e176a43efe3cfc67f048b5a448a47
Christian Murphy <Christian.Murphy.42@gmail.com>
2019-08-01 07:41:53 -0700

    types: export remark options type

--- packages/remark/types/index.d.ts
+++ packages/remark/types/index.d.ts
@@ -4,11 +4,13 @@ import unified = require('unified')
 import remarkParse = require('remark-parse')
 import remarkStringify = require('remark-stringify')
 
-type RemarkOptions = remarkParse.RemarkParseOptions &
-  remarkStringify.RemarkStringifyOptions
+declare namespace remark {
+  type RemarkOptions = remarkParse.RemarkParseOptions &
+    remarkStringify.RemarkStringifyOptions
+}
 
 declare function remark<
-  P extends Partial<RemarkOptions> = Partial<RemarkOptions>
+  P extends Partial<remark.RemarkOptions> = Partial<remark.RemarkOptions>
 >(): unified.Processor<P>
 
 export = remark
