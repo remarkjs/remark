f09c0baed3cae7771b4be76b5bc82339ae4c4efd
Jake Fried <samouri@users.noreply.github.com>
2019-12-09 12:17:12 -0500

    remark-parse: stricter link parsing for whitespace between display text and url

--- packages/remark-parse/lib/tokenize/link.js
+++ packages/remark-parse/lib/tokenize/link.js
@@ -107,18 +107,18 @@ function link(eat, value, silent) {
         depth--
       } else {
         // Allow white-space between content and url in GFM mode.
-        if (!pedantic) {
-          while (index < length) {
-            character = value.charAt(index + 1)
+        // if (!pedantic) {
+        //   while (index < length) {
+        //     character = value.charAt(index + 1)
 
-            if (!whitespace(character)) {
-              break
-            }
+        //     if (!whitespace(character)) {
+        //       break
+        //     }
 
-            subqueue += character
-            index++
-          }
-        }
+        //     subqueue += character
+        //     index++
+        //   }
+        // }
 
         if (value.charAt(index + 1) !== leftParenthesis) {
           return
